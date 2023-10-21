6024448b347263952b3529eba010aefbc8c167cb
Vladyslav6 <vlad51284@gmail.com>
2023-10-21 23:34:38 +0300

    finishTask1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
+const listcategory = document.querySelectorAll("#categories .item");
+console.log("Number of categories:", listcategory.length);
 
+const forCater = listcategory.forEach((number, index) => {
+  const findh2 = number.querySelector(".item h2");
+  console.log("Category:", findh2.textContent);
+
+  const liLength = number.querySelectorAll("li");
+  console.log(liLength.length);
+});
