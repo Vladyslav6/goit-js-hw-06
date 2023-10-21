59bcb70b7bc1497072cf195461f3fc2d6d0e387d
Vladyslav6 <vlad51284@gmail.com>
2023-10-22 00:58:32 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// document.createElement();
+const addlist = ingredients.forEach((Number) => {
+  const listcategory = document.querySelector("#ingredients .item");
+  console.log(Number);
+});
