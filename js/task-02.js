352d0e17777f5ab12c0e75d0b3c37149b4ad0c2a
Vladyslav6 <vlad51284@gmail.com>
2023-10-25 02:33:19 +0300

    finish

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,39 @@ const ingredients = [
   "Condiments",
 ];
 
-const Jora = document.getElementById("ingredients");
-ingredients.forEach((ingredient) => {
-  const JoraList = document.createElement("li");
-  JoraList.textContent = ingredient;
-  JoraList.classList.add("item");
-  Jora.appendChild(JoraList);
-});
+const UlIng = document.querySelector("#ingredients");
+const ingrList = ingredients
+  .map((ingr) => `<li class="item">${ingr}</li>`)
+  .join("");
+UlIng.innerHTML = ingrList;
+
+// ingredients.forEach((ingr) => {
+//   const Lilist = document.createElement("li");
+//   Lilist.classList.add("item");
+//   console.log(Lilist);
+// });
+
+// lastItem.textContent = "Poly";
+// UlIng.append(lastItem);
+// console.log(lastItem);
+
+// ingredients.forEach((ingr) => {
+//   console.log(ingr);
+//   Lilist.textContent = ingr;
+//   UlIng.prepend(Lilist);
+// });
+
+// Jora.append();
+
+// const Jora = document.getElementById("ingredients");
+// ingredients.forEach((ingredient) => {
+//   const JoraList = document.createElement("li");
+//   JoraList.textContent = ingredient;
+//   JoraList.classList.add("item");
+//   Jora.appendChild(JoraList);
+// });
+
+// const JoraList = document.createElement("li");
+// JoraList.textContent = "dds";
+// JoraList.classList.add("item");
+// Jora.appendChild(JoraList);
--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,13 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  if (event.currentTarget.value.length == 0) {
+  const OnTrim = event.currentTarget.value.trim();
+  if (OnTrim.length == 0) {
     output.textContent = "Anonymous";
   } else {
     output.textContent = event.currentTarget.value;
   }
 });
+
+// var string = "       Hello world!       ";
+// var result = string.trim(); // Hello world!
